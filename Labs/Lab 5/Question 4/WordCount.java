77927eca8c99e1a8ce71da46babeaece1a1a3b87
appventuremoment <107828563+appventuremoment@users.noreply.github.com>
2024-08-06 15:36:05 +0800

    Finished Lab 5 Chapter 6 part
    wow so done

--- Labs/Lab 5/Question 4/WordCount.java
+++ Labs/Lab 5/Question 4/WordCount.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.*;
 
 public class WordCount {
@@ -7,33 +9,56 @@ public class WordCount {
     // All words are stored in lowercase with no punctuation or spaces
     // Hint: Use regex to remove the punctuation from all words
     public WordCount(String filename){
-        // Complete the code below this comment
+        String temp = "";
 
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                temp += line + " ";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        temp = temp.replaceAll("[;,'.]", "");
+        dictionary = new HashMap<>();
+        for (String s: temp.split(" ")) {
+            dictionary.put(s.toLowerCase(), dictionary.getOrDefault(s.toLowerCase(), 0) + 1);
+        }
     }
 
     // Returns the count for a given word
     // If the word cannot be found, return 0
     public int getCount(String word){
-        // Complete the code below this comment
-        return 0;
+        if (hasWord(word)) return dictionary.get(word);
+        else return 0;
     }
 
     // Returns true if the dictionary has the word, false otherwise
     public boolean hasWord(String word){
-        // Complete the code below this comment
-        return false;
+        return dictionary.containsKey(word);
     }
 
     // Returns the word with the maximum count in the dictionary
     public String getWordWithMaxCount(){
-        // Complete the code below this comment
-        return null;
+        String ret = null;
+        int maxi = 0;
+
+        for (Map.Entry<String, Integer> entry : dictionary.entrySet()) {
+            if (entry.getValue() > maxi) {
+                maxi = entry.getValue();
+                ret = entry.getKey();
+            }
+        }
+        return ret;
     }
 
     // Returns an ArrayList of Strings of all unique words in the story.
-    public ArrayList<String> getAllUniqueWords(){
-        // Complete the code below this comment
-        return null;
+    public ArrayList<String> getAllUniqueWords() {
+        ArrayList temp = new ArrayList<>(dictionary.keySet());
+        Collections.sort(temp);
+        return temp;
     }
 
     @Override
